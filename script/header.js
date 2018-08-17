0f0854796361c12d4fd85526704eb35aab818dda
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-08-16 23:06:10 -0700

    added Vue component for repeating footer in each page

--- script/header.js
+++ script/header.js
@@ -2,9 +2,17 @@ Vue.component('nav-content',{
 	template: '<div><a href="index.html">Home</a><a href="#">Research</a><a href="energy-education.html">Education</a><a href="#">Outreach</a><a href="#">User Facilities</a><a href="industry.html">Energy Industry</a><a href="#">Advisory Board</a><a href="#">Contact Us</a></div>'
 })
 
+Vue.component('footer-content', {
+	template: '<div><i class="fab fa-facebook"></i><i class="fab fa-twitter-square"></i><i class="fab fa-linkedin"></i></div>'
+})
+
 document.addEventListener("DOMContentLoaded", function() {
 
 	let headerApp = new Vue({
 		el: '#headerApp'
+	})
+	
+	let footerApp = new Vue({
+		el: '#footerApp'
 	})
 	})
